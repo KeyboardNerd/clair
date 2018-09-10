3efe5a84d3b869b1e682bf964daf67266889cb9b
Sida Chen <git@sidac.me>
2018-09-10 11:25:01 -0400

    Add tests for V3 API

---
+++ api/v3/rpc_test.go
@@ -0,0 +1,43 @@
+// Copyright 2018 clair authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v3_test
+
+import (
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	// initialize the mock database
+}
+
+func TestPostAncestryExisting(t *testing.T) {
+
+}
+
+func TestPostAncestryNonExisting(t *testing.T) {
+
+}
+
+func TestGetAncestry(t *testing.T) {
+
+}
+
+func TestGetNotification(t *testing.T) {
+
+}
+
+func MarkNotificationAsRead(t *testing.T) {
+
+}
